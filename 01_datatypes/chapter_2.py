3cd4f0eab32ff3ce5ae7e114010ee8b1fdcaddc9
Mayank <vijaymaynk2003@gmail.com>
2025-07-12 23:30:41 +0530

    Add chapter_2.py to demonstrate spice mix operations

---
+++ 01_datatypes/chapter_2.py
@@ -0,0 +1,9 @@
+spice_mix = set()
+print(f"Intitial spice mix id: {id(spice_mix)}")
+print(f"Intitial spice mix id: {spice_mix}")
+
+spice_mix.add("Ginger")
+spice_mix.add("cardamon")
+
+print(f"After spice mix id: {id(spice_mix)}")
+print(f"Intitial spice mix id: {spice_mix}")
