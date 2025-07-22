dfa98821c1680361f3654fdb4d3d5e9c2dc60fce
Mayank <vijaymaynk2003@gmail.com>
2025-07-22 23:29:14 +0530

    Splitting Complex Tasks

---
+++ 04_functions/02_complex.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+    print("Fetching the sales data")
+    
+def filter_valid_sales():
+    print("Filtering valid sales data")
+    
+def summarize_data():
+    print("Summarizing sales data")
+    
+def genrate_repote():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+    print("Report is ready")
+
+genrate_repote()
