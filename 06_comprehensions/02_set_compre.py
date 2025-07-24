51eeaa034920759e5106152667a0e3a558aac366
Mayank <vijaymaynk2003@gmail.com>
2025-07-24 14:36:40 +0530

    set compre

---
+++ 06_comprehensions/02_set_compre.py
@@ -0,0 +1,23 @@
+favourite_chais = [
+    "Masala Chai",
+    "Green Tea",
+    "Masala Chai",
+    "Lemon Tea",
+    "Green Tea",
+    "Elaichi Chai",
+]
+
+unique_chai = {chai for chai in favourite_chais  }
+# print(unique_chai)
+
+
+recipes = {
+    "Masala Chai": ["ginger", "cardamom", "clove"],
+    "Elaichi Chai": ["cardamom", "milk"],
+    "Spicy Chai": ["ginger", "black pepper", "clove"],
+}
+
+
+unique_spices = { spice for ingredients in recipes.values() for spice in ingredients}
+# unique_spices = { spice.capitalize() for ingredients in recipes.values() for spice in ingredients}
+print(unique_spices)
