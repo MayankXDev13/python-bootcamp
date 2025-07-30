7a8286688892a3aec2fa5c236e39624d212ddbed
Mayank <vijaymaynk2003@gmail.com>
2025-07-30 23:01:47 +0530

    understand the how create class and -> create object

---
+++ 09_opp/01_simple_class.py
@@ -0,0 +1,11 @@
+class Chai: # Create Chai
+    pass
+
+class ChaiTime():
+    pass
+
+print(type(Chai))
+
+ginger_tea = Chai() # Create object
+print(type(ginger_tea) is Chai)
+print(type(ginger_tea) is ChaiTime)
