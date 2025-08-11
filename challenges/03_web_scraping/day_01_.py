4c545cd40255fa759625a5ef3a05b192da2199d0
Mayank <vijaymaynk2003@gmail.com>
2025-08-11 23:36:20 +0530

    day_01 -> Scrape Wikipedia h2 Headers day_02.py -> Hacker News Top Posts Scraper day_03.py -> Scrape Books To Scrape (70 Books)

---
+++ challenges/03_web_scraping/day_01_.py
@@ -0,0 +1,54 @@
+"""
+ Challenge: Scrape Wikipedia h2 Headers
+
+Use the `requests` and `BeautifulSoup` libraries to fetch the Wikipedia page on Python (programming language).
+
+Your task is to:
+1. Download the HTML of the page.
+2. Parse all `<h2>` section headers.
+3. Store the clean header titles in a list.
+4. Print the total count and display the first 10 section titles.
+
+Bonus:
+- Remove any trailing "[edit]" from the headers.
+- Handle network errors gracefully.
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+
+
+def get_h2_headers(url):
+    try:
+        reponse = requests.get(url, timeout=10)
+        reponse.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Failed to fetch page: \n {e}")
+        return []
+    
+    soup = BeautifulSoup(reponse.text, "html.parser")
+    h2_tags = soup.find_all("h2")
+    
+    # print(h2_tags)
+    
+    headers = []
+
+    for tag in h2_tags:
+        header_text = tag.getText(strip=True)
+        if header_text and header_text.lower() != "contents":
+            headers.append(header_text)
+    return headers
+    
+def main():
+    headers = get_h2_headers(URL)
+    
+    for header in headers[:10]:
+        print(header)
+    
+    
+if __name__ == "__main__":
+    main()
+    
